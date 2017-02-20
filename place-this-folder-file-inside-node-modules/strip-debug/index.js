09b79d86a8d72dc1922f33bb7a2e832c63d295ac
song xin <draftdog@foxmail.com>
2017-02-20 17:40:04 +0800

    这个文件夹是用来删除所有console用的 扔到node_module 替换

---
+++ place-this-folder-file-inside-node-modules/strip-debug/index.js
@@ -0,0 +1,19 @@
+'use strict';
+var rocambole = require('rocambole');
+var stripDebugger = require('rocambole-strip-debugger');
+var stripConsole = require('rocambole-strip-console');
+var stripAlert = require('rocambole-strip-alert');
+
+// esprima@2.1 introduces a "handler" property on TryStatement, so we would
+// loop the same node twice (see jquery/esprima/issues/1031 and #264)
+rocambole.BYPASS_RECURSION.handler = true;
+
+module.exports = function (src, options) {
+    options = options || {};
+
+    return rocambole.moonwalk(src, function (node) {
+        options.debugger !== false && stripDebugger(node);
+        options.console !== false && stripConsole(node);
+        options.alert !== false && stripAlert(node);
+    });
+};
